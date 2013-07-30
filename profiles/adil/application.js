b8afd28cce61b5c515d397e4b8db91173ab176ac
Mohammad Adil <mmadil_14@yahoo.com>
2013-07-31 03:37:42 +0530

    playing with jQuery

--- profiles/adil/application.js
+++ profiles/adil/application.js
@@ -2,16 +2,25 @@ jQuery(document).ready(function(){
 
   $('#about').click(function() {
     $('.about').show("fast");
+    $('.nav a').last().removeClass("active");
+    $('.nav a').first().next().removeClass("active");
+    $('.nav a').first().addClass("active");
     $('.contact').hide();
     $('.tech').hide();
   });
   $('#tech').click(function() {
     $('.tech').show("fast");
+    $('.nav a').first().removeClass("active");
+    $('.nav a').last().removeClass("active");
+    $('.nav a').first().next().addClass("active");
     $('.about').hide();
     $('.contact').hide();
   });
   $('#contact').click(function() {
     $('.contact').show("fast");
+    $('.nav a').first().removeClass("active");
+    $('.nav a').first().next().removeClass("active");
+    $('.nav a').last().addClass("active");
     $('.about').hide();
     $('.tech').hide();
   });
